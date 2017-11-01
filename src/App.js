72e29686556710d1582221627f392f339642d306
leomcelroy <leomcelroy@gmail.com>
2017-11-01 15:19:21 -0400

    merged work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 //adopted some from https://pspdfkit.com/blog/2017/how-to-build-free-hand-drawing-using-react/
 
 import React, { Component } from 'react';
+import {Line, Polygon} from './Shape.js';
+//import ReactDOM from 'react-dom';
 // import makerjs from 'makerjs';
 
 // let line = {
@@ -15,7 +17,7 @@ import React, { Component } from 'react';
 //   border: "1px solid red"
 // }
 
-const LOCK_DISTANCE = 7;
+const SELECT_DISTANCE = 7;
 
 class DrawingLine extends React.Component {
 
@@ -38,20 +40,26 @@ class DrawingLine extends React.Component {
 class Drawing extends React.Component {
 
   render() {
-    let lineArray = this.props.lines;
-    //console.log("lineArray:", lineArray);
+    let shapeArray = this.props.shapes;
+    let lineArray = [];
+    shapeArray.forEach((shape) => {
+      lineArray = lineArray.concat(shape.toLines());
+    });
+
+    lineArray = lineArray.concat(this.props.lines);
 
     let style = {
       width: "100%",
       height: "100%",
     }
-
-    return (
-      <svg style={style}>
+    let drawing = <svg style={style}>
         {lineArray.map((line, index) => (
           <DrawingLine key={index} line={line} />
         ))}
       </svg>
+
+    return (
+      drawing
     )
   }
 }
@@ -65,6 +73,8 @@ class DrawArea extends React.Component {
       tool: undefined,
       lines: [], //will be a list of lists
       start: undefined,
+      shapes: [], //will be a list of shapes
+      selected: undefined, //will be whatever object is 'selected'
     };
     this.handleMouseDown = this.handleMouseDown.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
@@ -72,22 +82,11 @@ class DrawArea extends React.Component {
   }
 
   handleMouseDown(mouseEvent) {
-    // console.log("mouse down!");
     if (mouseEvent.button !== 0) {
       return;
     }
     var point = this.relativeCoordinatesForEvent(mouseEvent);
-    if (this.state.tool === "POLYGON"
-        && this.state.start
-        && this.distanceSquared(point, this.state.start) < LOCK_DISTANCE**2 ) {
-      point = this.state.start;
-    }
-    console.log(this.state.isDrawing);
-    if (!this.state.isDrawing) {
-      this.setState({
-        start: point
-      });
-    }
+    let oldShapes = this.state.shapes;
 
     switch (this.state.tool) {
       case "FREEHAND":
@@ -99,35 +98,70 @@ class DrawArea extends React.Component {
 
         let newState = oldState;
 
-        // console.log("new: ", newState)
+        console.log("new: ", newState)
 
         this.setState({
             lines: newState,
             isDrawing: true,
         });
         break;
-      case "LINE":  //fallthrough
-      case "POLYGON":
-        if (this.state.isDrawing && this.state.tool === "LINE") {
+      case "LINE":
+        if (this.state.isDrawing) {
           this.setState({
             isDrawing: false
-          });
+          })
         } else {
-          let oldLines = this.state.lines;
-          oldLines.push([point, point]);
-          let newLines = oldLines.slice(0, oldLines.length);
-          console.log(oldLines);
-          console.log(newLines);
+          let line = Line(point);
+          oldShapes.push(line);
           this.setState({
-            lines: oldLines,
-            isDrawing: point !== this.state.start,  //stop drawing if polygon is complete
+            shapes: oldShapes,
+            isDrawing: true,
           });
+          console.log("shapes", this.state.shapes);
         }
         break;
+      case "POLYGON":
+          if (this.state.isDrawing) {
+            if (oldShapes[oldShapes.length -1].closed()) {
+              console.log('closed');
+              this.setState({
+                isDrawing: false,
+              });
+            } else {
+              console.log('not closed');
+              oldShapes[oldShapes.length-1].addPoint(point);
+              this.setState({
+                shapes: oldShapes,
+                //isDrawing: true, //this is redundant, but i may refactor later
+              })
+            }
+          } else {
+            let polygon = Polygon(point);
+
+            oldShapes.push(polygon);
+            this.setState({
+              shapes: oldShapes,
+              isDrawing: true,
+            });
+          }
+        break;
+      case "SELECT":
+        let selected = undefined;
+        this.state.shapes.forEach((shape) => {  //todo: refactor forEach to some?
+          let obj = shape.selectedObjectAt(point);
+          if (obj) {
+            selected = obj;
+            //break
+          }
+        });
+        this.setState({
+          selected: selected,
+        })
+      break;
       default:
         return;
     }
-
+ console.log('selected', this.state.selected);
 
   }
 
@@ -144,30 +178,47 @@ class DrawArea extends React.Component {
   }
 
   handleMouseMove(mouseEvent) {
+    var point = this.relativeCoordinatesForEvent(mouseEvent);
     if (!this.state.isDrawing) {
-      return;
+      switch (this.state.tool) {
+        case "SELECT":
+          this.state.lines.map((line, index) => {
+            var point = this.relativeCoordinatesForEvent(mouseEvent);
+            let d1 = this.distanceSquared(line[0], point)**(1/2);
+            let d2 = this.distanceSquared(line[1], point)**(1/2);
+            let total = this.distanceSquared(line[0], line[1])**(1/2);
+            // console.log(d1+d2);
+            // console.log("total", total)
+
+            //doesnt work for freehand lines
+            if ((d1 + d2) < (total+1) || (d1 + d2) < (total-1)) {
+              console.log(index);
+            }
+
+          })
+          break;
+        default:
+          return;
+      }
     }
 
     var point = this.relativeCoordinatesForEvent(mouseEvent);
-    if (this.state.tool === "POLYGON"
-        && this.state.start
-        && this.distanceSquared(point, this.state.start) < LOCK_DISTANCE**2 ) {
-      point = this.state.start;
-    }
+    let oldShapes = this.state.shapes;
     let oldState = this.state.lines;
+
     switch (this.state.tool) {
-      case "LINE": //fallthrough
+      case "LINE":
+
+        oldShapes[oldShapes.length - 1].p2(point); //update second point of line
+        this.setState({  //TODO: FACTOR THIS OUT OF ALL CASES?
+          shapes: oldShapes,
+        });
+      break;
+
       case "POLYGON":
-        var lastLine = oldState.pop();
-        lastLine[1] = point;
-
-        var temp = oldState.slice(0,oldState.length)
-        temp.push(lastLine);
-
-        var newState = temp;
-
+        oldShapes[oldShapes.length -1].lastPoint(point); //update the last point of the polygon
         this.setState({
-            lines: newState,
+          shapes: oldShapes,
         });
         break;
       case "FREEHAND":
@@ -195,6 +246,8 @@ class DrawArea extends React.Component {
 
   }
 
+  //TODO: REFACTOR ALL OF THESE INTO ONE
+
   onClickFreeHand() {
     this.setState({
         tool: "FREEHAND",
@@ -213,10 +266,26 @@ class DrawArea extends React.Component {
     });
   }
 
+  onClickSelect() {
+    this.setState({
+      tool: "SELECT",
+    });
+  }
+
   onClickNoTool() {
     this.setState({
         tool: undefined,
     });
+  }
+
+  toZero() { //assumes selected is a point
+    this.state.selected.y = this.state.selected.x = 0;
+    this.setState({}); //call render
+  }
+
+  horizontal() { //assumes selected is a line
+    this.state.selected.p2_.y = this.state.selected.p1_.y;
+    this.setState({}); //call render
   }
 
   componentDidMount() {
@@ -241,6 +310,47 @@ class DrawArea extends React.Component {
 
   }
 
+  // handleDownload() {
+  //   let svgString = `<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"> <line id="svg_1" y2="221" x2="246" y1="245" x1="107" stroke-width="5" stroke="#000000" fill="none"/>
+  //   </svg>`
+  //
+  //   let uriContent = "data:application/octet-stream," + encodeURIComponent(svgString);
+  //   let newWindow = window.open(uriContent, 'test');
+  // }
+
+  handleDownload() {
+    let filename = "test.svg";
+    let shapeArray = this.state.shapes;
+    let lineArray = [];
+    shapeArray.forEach((shape) => {
+      lineArray = lineArray.concat(shape.toLines());
+    });
+
+    lineArray = lineArray.concat(this.state.lines);
+
+    let svgString = lineArray.map(line => `<path d="M ${line.map(p => `${p['x']} ${p['y']}`)}" stroke-linejoin="round" stroke-linecap="round" stroke-width="1px" stroke="black" fill="none"/>`);
+
+    let text = `<svg
+      width="640"
+      height="480"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:svg="http://www.w3.org/2000/svg">
+      ${svgString.join("")}
+    </svg>`
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+  }
+
   render() {
     // console.log("drawing: ", this.state.isDrawing);
     // console.log("state: ",this.state.lines);
@@ -250,6 +360,7 @@ class DrawArea extends React.Component {
       border: "1px solid black",
       float: "left",
       cursor: "crosshair",
+      float: "left"
     }
 
     let activeButtonStyle = {
@@ -261,18 +372,29 @@ class DrawArea extends React.Component {
 
     return (
       <div>
-      <button style={this.state.tool === "FREEHAND" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickFreeHand(e)}>Free Hand</button>
-      <button style={this.state.tool === "LINE" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickLine(e)}>Line</button>
-      <button style={this.state.tool === "POLYGON" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickPolygon(e)}>Polygon</button>
-      <button style={this.state.tool === undefined ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickNoTool(e)}>No Tool</button>
         <div
           style={drawAreaStyle}
           ref="drawArea"
           onMouseDown={this.handleMouseDown}
           onMouseMove={this.handleMouseMove}
         >
-          <Drawing lines={this.state.lines} />
+          <Drawing lines={this.state.lines}
+                    shapes={this.state.shapes}/>
         </div>
+
+        <table style={{float:"left"}}>
+          <tbody>
+            <tr><td><button style={this.state.tool === "FREEHAND" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickFreeHand(e)}>Free Hand</button></td></tr>
+            <tr><td><button style={this.state.tool === "LINE" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickLine(e)}>Line</button></td></tr>
+            <tr><td><button style={this.state.tool === "POLYGON" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickPolygon(e)}>Polygon</button></td></tr>
+            <tr><td><button style={this.state.tool === "SELECT" ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickSelect(e)}>Select</button></td></tr>
+            <tr><td><button style={this.state.tool === undefined ? activeButtonStyle : inactiveButtonStyle} onClick={(e) => this.onClickNoTool(e)}>No Tool</button></td></tr>
+
+            <tr><td><button onClick={(e) => this.toZero(e)}>To Zero</button></td></tr>
+            <tr><td><button onClick={(e) => this.horizontal(e)}>Horizontal</button></td></tr>
+            <tr><td><button onClick={(e) => this.handleDownload(e)}>Download SVG</button></td></tr>
+          </tbody>
+        </table>
       </div>
     );
   }
---
+++ src/Shape.js
@@ -0,0 +1,132 @@
+//look at maker.js?
+
+function Line(p1, p2) {
+  if (!p2) {
+    p2 = p1;
+  }
+  let line = {
+    shape_ : 'line',
+    p1_: p1,
+    p2_: p2,
+  };
+  line.p1 = function(p) { if (p){ this.p1_ = p; return this;} return this.p1_};
+  line.p2 = function(p) { if (p){ this.p2_ = p; return this;} return this.p2_};
+  line.toLine = function() { return [this.p1_, this.p2_] };
+  line.toLines = function() { return [[this.p1_, this.p2_]] };
+  line.selectedObjectAt = function(point) { return undefined; } //TODO: IMPLEMENT
+  //TODO: ADD OTHER METHODS
+  return line;
+}
+
+function Polygon(point) {
+  let polygon = {
+    shape_ : 'polygon',
+    points_ : [],
+    lockDistance_ : 10,
+    pointSelectDistance_ : 15, //todo: setter / getter
+    lineSelectDistance_ : 14,
+  }
+
+  if (point) {
+    polygon.points_.push(point);
+    polygon.points_.push(point);
+  }
+
+  polygon.lockDistance = function(d) { if(d){ this.lockDistance_ = d; return this} return this.lockDistance_};
+
+  //polygon.addLine = function(line) {this.lines_.push(line); return this};
+  polygon.addPoint = function(point) {this.points_.push(point); return this;}
+  polygon.toLines = function() {
+    let lines = []
+    for (let i = 0; i < this.points_.length -1; i++) {
+      lines.push([this.points_[i], this.points_[i+1]]);
+    }
+    return lines;
+  };
+
+  polygon.withinLock = function(p) { return distanceSquared(this.points_[0], p) < this.lockDistance_**2 };
+
+  polygon.lastPoint = function(p) {  //if arg is specified, sets the last point of the polygon and returns this
+    if (!p) {
+      return this.points_[this.points_.length -1];
+    }
+
+    if (this.withinLock(p)) {
+      p = {'x': this.points_[0].x, 'y': this.points_[0].y}; //create a new point so we don't get double referencing
+    }
+    this.points_[this.points_.length -1] = p;
+    return this;
+  }
+  polygon.closed = function() {return pointEqual(this.points_[0], this.lastPoint()) };
+
+  polygon.selectedObjectAt = function(point) {
+    //try points
+    for (var i=0; i < this.points_.length; i++) {
+      let d1 = distanceSquared(point, this.points_[i]);
+      if (d1 < this.pointSelectDistance_**2) { console.log('point!')
+        return this.points_[i];
+      }
+    }
+    //try lines
+    for (var i=0; i < this.points_.length-1; i++) {
+      let d1 = distanceSquared(point, this.points_[i]);
+      let d2 = distanceSquared(point, this.points_[i+1]);
+      if (Math.sqrt(d1) + Math.sqrt(d2) <
+          Math.sqrt(distanceSquared(this.points_[i], this.points_[i+1])) + this.lineSelectDistance_) {
+        return Line(this.points_[i], this.points_[i+1]);
+      }
+    }
+  }
+
+  return polygon;
+} //end Polygon()
+
+function LineAngleConstraint(line, angle) { //a quick example?
+
+}
+
+function distanceSquared(p1, p2) {
+  return (p1.x - p2.x)**2 + (p1.y - p2.y)**2;
+}
+
+function pointEqual(p1, p2) {
+  let foo = p1.x === p2.x && p1.y == p2.y;
+  return foo;
+}
+
+// function makeShape(type) {
+//
+// }
+//
+// function Line(foo) {
+//   let obj = {
+//     p1: foo,
+//     p2: foo,
+//     pred: undefined,
+//     succ: undefined,
+//   }
+//     obj.moveEnd = (newPoint) => {this.p2 = newPoint;};
+//     //TODO: CACHE RESULT OF RENDER
+//     obj.angle = () => {Math.atan2(this.p1.y - this.p2.y, this.p1.x - this.p2.x);}; //how to make these parametric?
+//     obj.length = () => {Math.sqrt((this.p1.y - this.p2.y)**2 + (this.p1.x - this.p2.x)**2);}; //probably with different functions
+//     obj.render = () => {console.log(this); [{'x': obj.p1.x, 'y': obj.p1.y}, {'x': obj.p2.x, 'y': obj.p2.y}]; };
+//
+//     return obj;
+// }
+//
+// // function PolyLine() {
+// //   return {
+// //     lines: [],
+// //     addLine: (line) => {
+// //       try {
+// //         lines[lines.length - 1].succ = line;
+// //         line.pred = lines[lines.length - 1];
+// //       }
+// //       catch (e) {}
+// //       lines.push(line);
+// //     },
+// //   }
+// // }
+
+
+export {Line, Polygon};
